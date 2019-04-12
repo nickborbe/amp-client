57439f0f02ec7e96bee1f380083d554fe9b49171
nickborbe <nickborbe@gmail.com>
2019-04-12 19:18:49 -0400

    navbar

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -18,7 +18,10 @@ class HomePage extends Component {
           <img/>
         </header>
         <nav>
-          
+          <a href="#"></a>
+          <a href="#"></a>
+          <a href="#"></a>
+          <a href="#"></a>
         </nav>
 
    
