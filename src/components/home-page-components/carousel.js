39747256f510464ce3e934d3ffd8b0e266b5211c
nickborbe <nickborbe@gmail.com>
2019-04-18 15:10:52 -0400

    carousel works

--- src/components/home-page-components/carousel.js
+++ src/components/home-page-components/carousel.js
@@ -8,21 +8,55 @@ import '../../styles/css/all.css'
 
 
 class Carousel extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            images: ["/images/gavel.jpg", "/images/carousel-2.png", "/images/carousel-3.png", "/images/on-a-boat.jpg", "/images/carousel-2.png", "/images/carousel-1.png"],
+            temp: "",
+           
+        }
+    }
+
+
+   scroll(direction){
+       const tempArray = this.state.images.slice();
+       let source = "";
+    if(direction ==="right"){
+        source = this.state.images[0];
+        tempArray.shift();
+        tempArray.push(source);
+        this.setState({temp: source, images: tempArray})
+
+    } else{
+
+        source = this.state.images[this.state.images.length - 1];
+        tempArray.pop();
+        tempArray.unshift(source);
+        this.setState({temp: source, images: tempArray})
+    }
+
+   
+   
+  
+    }
+
+
   render() {
       return(
 
 
         <div id="hp-carousel-ads">
 
-        <i class="fas fa-angle-left"></i>
+        <i className="fas fa-angle-left" onClick={()=>{this.scroll('left')}}></i>
 
-        <img src="/images/carousel-1.png" />
+        <img src={this.state.images[0]} />
 
-        <img src="/images/carousel-2.png" />
+        <img src={this.state.images[1]} />
 
-        <img src="/images/carousel-3.png" />
+        <img src={this.state.images[2]} />
 
-        <i class="fas fa-angle-right"></i>
+        <i className="fas fa-angle-right"  onClick={()=>{this.scroll('right')}} ></i>
 
         </div>
         
