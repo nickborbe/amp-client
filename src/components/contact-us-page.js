030f7d4d23ee82ce52ed8d42b38ef5e77ce132e3
nickborbe <nickborbe@gmail.com>
2019-05-12 15:00:12 -0400

    contact design

--- src/components/contact-us-page.js
+++ src/components/contact-us-page.js
@@ -15,8 +15,47 @@ class ContactUsPage extends Component {
 
       <div>
 
+        <div className="p-wrapper">
+    <p>Lorem ipsum dolor amet cred yr pour-over banjo sriracha, artisan church-key cronut bicycle rights. Fanny pack quinoa sustainable, affogato unicorn cred tacos 8-bit keytar paleo hexagon. Normcore wayfarers everyday kogi.</p>
+        </div>
+
+
+        <div className="orange-form">
+            <form>
+
+        <div className="fieldSet">
+                <label>CONTACT US- DROP DOWN TO SELECT SUBJECT</label>
+                <select>
+                    <option>FAN MAIL</option>
+                    <option>QUESTION/ANSWER</option>
+                    <option>RE: CONTEST</option>
+                </select>
+        </div>
+
+
+        <div className="fieldSet">
+            <label>NAME</label>
+            <input placeholder="John Doe"></input>
+        </div>
+
+        <div className="fieldSet">
+            <label>EMAIL</label>
+            <input placeholder="JohnDoe@email.com"></input>
+        </div>
+
+        <div className="fieldSet">
+            <label>MESSAGE</label>
+            <textarea></textarea>
+        </div>
+
+
+
+            <button>SEND</button>
+            </form>
+        </div>
+
  
-BOO!
+
 
         
 
