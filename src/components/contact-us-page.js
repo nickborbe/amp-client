bf5910c8a0051c464edfee264674b24b4796477b
nickborbe <nickborbe@gmail.com>
2019-05-13 10:11:24 -0400

    react dropdowns

--- src/components/contact-us-page.js
+++ src/components/contact-us-page.js
@@ -10,6 +10,18 @@ import SponsoredAd from './layout-components/sponsored-ad'
 
 
 class ContactUsPage extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            answerShowing: false
+        }
+    }
+
+    showAnswer(num){
+        this.setState({answerShowing: num})
+    }
+
+
   render() {
     return (
 
@@ -61,40 +73,46 @@ class ContactUsPage extends Component {
 
         <h2> FREQUENTLY ASKED QUESTIONS</h2>
         
-        <div className="faq-drop">
+        <div className="faq-drop" onClick={()=>{this.showAnswer(0)}}>
          <span>
         THIS IS A QUESTION THAT IS OFTEN ASKED ON THIS WEBSITE.  PLEASE TAKE A MOMENT TO READ
         </span>
-        <i className="fas fa-chevron-down"></i>
+        <i className="fas fa-chevron-down" ></i>
         </div>
+        {
+            this.state.answerShowing === 0 &&  <div className="answer" id="an1">
+            This is the answer to the question tiam hendrerit nulla sit amet diam faucibus, facilisis ultricies magna vulputate. Vestibulum sit amet pharetra nulla, a interdum ligula. Maecenas finibus purus libero, at consequat neque consequat a. Vestibulum quis euismod eros. Vestibulum ut quam iaculis, maximus purus quis, ornare nunc. Sed vitae quam augue. In fringilla ornare nibh, in venenatis lacus euismod ac. Maecenas nec odio ac metus vulputate placerat. Nulla facilisi. Suspendisse sit amet euismod nunc. Nam tincidunt pulvinar erat, vitae fermentum augue euismod ultricies. Proin a purus at lorem vulputate porttitor id eu felis. Etiam maximus orci urna, eu mollis lacus suscipit nec. Donec at purus metus.
+            </div>
+        }
+       
 
-        <div className="faq-drop">
+        <div className="faq-drop" onClick={()=>{this.showAnswer(1)}}>
          <span>
         THIS IS A QUESTION THAT IS OFTEN ASKED ON THIS WEBSITE.  PLEASE TAKE A MOMENT TO READ
         </span>
         <i className="fas fa-chevron-down"></i>
         </div>
+        {
+            this.state.answerShowing === 1 &&  <div className="answer" id="an2">
+            This is the answer to the question tiam hendrerit nulla sit amet diam faucibus, facilisis ultricies magna vulputate. Vestibulum sit amet pharetra nulla, a interdum ligula. Maecenas finibus purus libero, at consequat neque consequat a. Vestibulum quis euismod eros. Vestibulum ut quam iaculis, maximus purus quis, ornare nunc. Sed vitae quam augue. In fringilla ornare nibh, in venenatis lacus euismod ac. Maecenas nec odio ac metus vulputate placerat. Nulla facilisi. Suspendisse sit amet euismod nunc. Nam tincidunt pulvinar erat, vitae fermentum augue euismod ultricies. Proin a purus at lorem vulputate porttitor id eu felis. Etiam maximus orci urna, eu mollis lacus suscipit nec. Donec at purus metus.
+            </div>
+        }
+     
 
-        <div className="faq-drop">
+        <div className="faq-drop" onClick={()=>{this.showAnswer(2)}}>
          <span>
         THIS IS A QUESTION THAT IS OFTEN ASKED ON THIS WEBSITE.  PLEASE TAKE A MOMENT TO READ
         </span>
-        <i className="fas fa-chevron-down"></i>
+        <i className="fas fa-chevron-down" ></i>
         </div>
+        {
+            this.state.answerShowing === 2 &&  <div className="answer" id="an3">
+            This is the answer to the question tiam hendrerit nulla sit amet diam faucibus, facilisis ultricies magna vulputate. Vestibulum sit amet pharetra nulla, a interdum ligula. Maecenas finibus purus libero, at consequat neque consequat a. Vestibulum quis euismod eros. Vestibulum ut quam iaculis, maximus purus quis, ornare nunc. Sed vitae quam augue. In fringilla ornare nibh, in venenatis lacus euismod ac. Maecenas nec odio ac metus vulputate placerat. Nulla facilisi. Suspendisse sit amet euismod nunc. Nam tincidunt pulvinar erat, vitae fermentum augue euismod ultricies. Proin a purus at lorem vulputate porttitor id eu felis. Etiam maximus orci urna, eu mollis lacus suscipit nec. Donec at purus metus.
+            </div>
+        }
+     
 
-        <div className="faq-drop">
-         <span>
-        THIS IS A QUESTION THAT IS OFTEN ASKED ON THIS WEBSITE.  PLEASE TAKE A MOMENT TO READ
-        </span>
-        <i className="fas fa-chevron-down"></i>
-        </div>
-        
-        <div className="faq-drop">
-         <span>
-        THIS IS A QUESTION THAT IS OFTEN ASKED ON THIS WEBSITE.  PLEASE TAKE A MOMENT TO READ
-        </span>
-        <i className="fas fa-chevron-down"></i>
-        </div>
+
 
         </section>
  
